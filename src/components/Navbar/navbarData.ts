93037b10af566fc9fe5e59f137a8e7db088d7827
AdrianDanao <adrian.danao22@gmail.com>
2025-07-02 12:28:27 +0800

    Removing unused buttons

--- src/components/Navbar/navbarData.ts
+++ src/components/Navbar/navbarData.ts
@@ -1,31 +1,15 @@
 import {
-  DocumentTextIcon,
   HomeIcon,
-  BuildingOfficeIcon,
-  BuildingOffice2Icon,
   UserGroupIcon,
 } from "@heroicons/react/24/outline";
 
 export const actions = {
   Intern: [
     { label: "Home", to: "/home", icon: HomeIcon },
-    {
-      label: `Major Dept.`,
-      to: "/department/major",
-      icon: BuildingOfficeIcon,
-    },
-    {
-      label: "Minor Dept.",
-      to: "/department/minor",
-      icon: BuildingOffice2Icon,
-    },
-    { label: "Reports", to: "/reports", icon: DocumentTextIcon },
   ],
   "Department Head": [
     { label: "Home", to: "/home", icon: HomeIcon },
-    { label: "Dept.", to: "/tasks", icon: BuildingOfficeIcon },
     { label: "Intern List", to: "/interns", icon: UserGroupIcon },
-    { label: "Daily Reports", to: "/reports/daily", icon: DocumentTextIcon },
   ],
   //   Supervisor: [
   //     { label: "Intern List", to: "/interns", icon: "👥" },
